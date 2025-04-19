7c365961e848fb22b680ac7b5f396ca2aebfa4cc
shtrohaenjoer <fanalina64@gmail.com>
2025-04-20 00:06:32 +0300

    новая фи4а

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Люблю Иисуса</h1>
+        <h2>Люблю Иисуса</h2>
       </main>
       <footer className={styles.footer}>
         <a
