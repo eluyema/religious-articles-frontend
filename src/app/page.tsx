7523c410ce7f08b7c206beb8eca5ecfb0bd95ccd
Hermanovych Danylo <elluyema@gmail.com>
2025-05-07 01:58:51 +0300

    remove extra next.js code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,23 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h2>Люблю Иисуса</h2>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <h2>I Love Jesus</h2>
+          <p>
+            Welcome to <strong>Jesus Near</strong> — a place where you can explore the love and teachings of Jesus Christ.
+            We believe that true peace and salvation are found in Him alone.
+          </p>
+          <p>
+            Here you will find Christian articles, prayers, and spiritual reflections to help you grow in your faith, understand the Bible,
+            and walk closer with the Lord every day.
+          </p>
+          <p>
+            Jesus is near to all who call upon Him in truth (Psalm 145:18). Whether you&#39;re a believer or just searching for truth, we invite
+            you to discover the good news of the Gospel and the hope we have in Jesus Christ.
+          </p>
+        </main>
+      </div>
   );
 }
