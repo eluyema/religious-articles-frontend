231c11b5181e3119e94ce5ce6a5cc4c8665ba25c
Hermanovych Danylo <elluyema@gmail.com>
2025-05-07 11:18:30 +0300

    update favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,6 @@ export default function RootLayout({
   return (
       <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any"/>
         <meta name="theme-color" content="#ffffff"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </head>
