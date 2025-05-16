8aa1e00d5473c0cbaa1ca8f973c110f44e19dae8
Hermanovych Danylo <elluyema@gmail.com>
2025-05-16 13:42:45 +0300

    fix page static generate

--- src/app/[locale]/articles/[category]/page.tsx
+++ src/app/[locale]/articles/[category]/page.tsx
@@ -1,17 +1,20 @@
-
 import { Metadata } from 'next';
-//
-// import {Article} from "@/features/articles/model/entities";
-
-export async function generateStaticParams() {
-    const categories = categoriesConfig.map(({code})=>code);
-
-    return categories.map((category) => ({ category }));
-}
-
-import { getTranslations} from 'next-intl/server';
+import { getTranslations } from 'next-intl/server';
 import CategoryArticleListPage from "@/features/articles/ui/CategoryArticleListPage";
-import {categoriesConfig} from "@/shared/config/categoriesConfig";
+import { categoriesConfig } from "@/shared/config/categoriesConfig";
+
+export function generateStaticParams() {
+    const categories = categoriesConfig.map(({ code }) => code);
+
+    const locales = ['en', 'es', 'de', 'fr', 'pt', 'ru'];
+
+    return locales.flatMap(locale =>
+        categories.map(category => ({
+            locale,
+            category
+        }))
+    );
+}
 
 type Props = {
     params: Promise<{
@@ -20,36 +23,23 @@ type Props = {
     }>;
 };
 
-export async function generateMetadata({ params }: {
-    params: Promise<{
-        category: string;
-        locale: string;
-    }>;
-}): Promise<Metadata> {
-    const t = await getTranslations('categoriesArticles');
-    const {category} = await params;
+export async function generateMetadata({ params }: { params: Promise<{ category: string; locale: string }>; }): Promise<Metadata> {
+    const {locale, category} = await params;
+    const t = await getTranslations({ locale, namespace: 'categoriesArticles' });
+
     return {
         title: t(`${category}.metaTitle`),
-        description: t(`${category}.metaDescription`),
+        description: t(`${category}.metaDescription`)
     };
 }
 
-export default async function Page({ params }: Props) {
-    // const articles: Article[] = await fetch(
-    //     `http://localhost:8080/api/christianity/client/articles?category=${params.category}`,
-    //     {
-    //         next: { revalidate: 60 }, // or `cache: 'no-store'` for SSR
-    //         headers: {
-    //             "X-CLIENT-SECRET": process.env.NEXT_SECRET_KEY || "",
-    //         }
-    //     }
-    // );
 
-    // const articles = await res.json();
-
+const Page = async ({ params }: Props) => {
     const { category } = await params;
 
     return (
-        <CategoryArticleListPage category={category} articles={[]}/>
+        <CategoryArticleListPage category={category} articles={[]} />
     );
-}
+};
+
+export default Page;
