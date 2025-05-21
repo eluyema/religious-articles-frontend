93d580f1f7a6d7b01a60ed91d87fe9c8772eb777
Hermanovych Danylo <elluyema@gmail.com>
2025-05-21 12:12:49 +0300

    add default icon to layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -46,6 +46,7 @@ export default async function RootLayout({children, params
             <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="icon" href="/favicon-32x32.png" type="image/png" />
             <link rel="manifest" href="/manifest.json"/>
 
         </head>
