f490a5dca8008566fa9f88f838787a16a5842aec
Hermanovych Danylo <elluyema@gmail.com>
2025-05-22 17:24:37 +0300

    update switcher logic

--- src/shared/ui/LocaleSwitcher/LocaleSwitcher.tsx
+++ src/shared/ui/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,8 +1,9 @@
 'use client';
 import styles from './index.module.scss';
 
-import { useRouter, usePathname } from 'next/navigation';
+import {useParams, usePathname, useRouter} from 'next/navigation';
 import {supportedLocales, localeLabels, defaultLocale} from '@/shared/config/supportedLocales';
+
 import classNames from 'classnames';
 
 type LocaleSwitcherProps = {
@@ -11,9 +12,14 @@ type LocaleSwitcherProps = {
 
 const LocaleSwitcher = ({ className = '' }: LocaleSwitcherProps) => {
     const router = useRouter();
-    const pathname = usePathname();
+    const params = useParams();
 
-    const currentLocale = supportedLocales.find(locale => pathname.startsWith(`/${locale}/`)) || defaultLocale;
+    // Correctly extract current locale from the route param
+    const currentLocale = typeof params?.locale === 'string' && supportedLocales.includes(params.locale)
+        ? params.locale
+        : defaultLocale;
+
+    const pathname = usePathname();
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const newLocale = e.target.value;
