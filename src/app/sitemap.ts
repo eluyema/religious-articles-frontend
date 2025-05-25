3a9ea9a3ab5c77e7680fc7b97bdb2cb814a1ffde
Hermanovych Danylo <elluyema@gmail.com>
2025-05-25 15:43:28 +0300

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,23 +3,34 @@ import { supportedLocales } from "@/shared/config/supportedLocales";
 import { categoriesConfig } from "@/shared/config/categoriesConfig";
 import { loadAllArticlePath } from "@/features/articles/api/endpoints/loadAllArticlePath";
 
+const getBase = (locale: string) => {
+    if(locale === "en") return "https://www.jesusnear.com";
+
+    return `https://www.jesusnear.com/${locale}`;
+}
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const allPath = await loadAllArticlePath();
 
     const articlesUrls: MetadataRoute.Sitemap = allPath
         .filter(({ active }) => active)
-        .map(({ slug, language, category, subcategory, updatedAt }) => ({
-            url: `https://www.jesusnear.com/${language === 'en' ? '' : language}/articles/${category}/${subcategory}/${slug}`,
+        .map(({ slug, language, category, subcategory, updatedAt }) => {
+            const base = getBase(language);
+
+            return ({
+            url: `${base}/articles/${category}/${subcategory}/${slug}`,
             lastModified: new Date(updatedAt),
             changeFrequency: 'monthly',
             priority: 0.4,
-        }));
+        })});
 
     const now = new Date();
 
     const staticUrls: MetadataRoute.Sitemap = supportedLocales.flatMap(locale => {
+        const baseUrl = getBase(locale);
+
         const categoryUrls = categoriesConfig.map(category => ({
-            url: `https://www.jesusnear.com/${locale === 'en' ? '' : locale}/articles/${category.code}`,
+            url: `${baseUrl}/articles/${category.code}`,
             lastModified: now,
             changeFrequency: 'weekly' as const,
             priority: 0.8,
@@ -27,7 +38,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
         const urls = [
             {
-                url: `https://www.jesusnear.com/${locale === 'en' ? '' : locale}`,
+                url: baseUrl,
                 lastModified: now,
                 changeFrequency: 'weekly' as 'weekly' | 'monthly',
                 priority: 1,
@@ -35,22 +46,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             ...categoryUrls,
         ];
 
-        // Only include privacy policy if not already removed — and adjust for default locale
-        if (locale !== 'en') {
+
             urls.push({
-                url: `https://www.jesusnear.com/${locale}/privacy-policy`,
+                url: `${baseUrl}/privacy-policy`,
                 lastModified: now,
                 changeFrequency: 'monthly' as const,
                 priority: 0.5,
             });
-        } else {
-            urls.push({
-                url: `https://www.jesusnear.com/privacy-policy`,
-                lastModified: now,
-                changeFrequency: 'monthly' as const,
-                priority: 0.5,
-            });
-        }
 
         return urls;
     });
