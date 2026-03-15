4489503e0a642841734dbc42427f299dfe4735c4
Hermanovych Danylo <elluyema@gmail.com>
2026-03-15 14:48:02 +0200

    add google adsense script to header fix

--- src/core/providers/HtmlLayoutWrapper/HtmlLayoutWrapper.tsx
+++ src/core/providers/HtmlLayoutWrapper/HtmlLayoutWrapper.tsx
@@ -12,16 +12,21 @@ type LayoutWrapperProps = {
     children: React.ReactNode;
 };
 
+const ADSENSE_SCRIPT_SRC =
+    'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7752563209985738';
+
 export default function HtmlLayoutWrapper({ locale, children }: LayoutWrapperProps) {
     return (
         <html lang={locale}>
+        <head>
+            <script
+                async
+                src={ADSENSE_SCRIPT_SRC}
+                crossOrigin="anonymous"
+            />
+        </head>
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ResourceHints />
-        <Script
-            strategy="beforeInteractive"
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7752563209985738"
-            crossOrigin="anonymous"
-        />
         <Script
             strategy="afterInteractive"
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
